f34e0bb564995af477b4ecbb540b891fe93c001f
suryansh tyagi <131903332+utkarshx08@users.noreply.github.com>
2026-08-11 17:03:26 +0530

    Time: 1 ms (6.91%) | Memory: 19.3 MB (61.40%) - LeetSync

---
+++ 198-house-robber/house-robber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        prev2 = 0
+        prev1 = 0
+
+        for money in nums:
+
+            current = max(
+                prev1,
+                prev2 + money
+            )
+
+            prev2 = prev1
+            prev1 = current
+
+        return prev1
